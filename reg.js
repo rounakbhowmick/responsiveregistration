bf460cdbfbb0e45eb308129c6a3841ee760a094b
Rounak Bhowmick <rounakchakda@gmail.com>
2020-04-20 18:56:21 +0530

    width issue

---
+++ reg.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('.signup-form');
+const form = document.querySelector('.signup-form');
+const name = do cument.querySelector('#name')
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+
+    });
