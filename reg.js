d7c0a8e4c6ad5345936bed52d1e19dedaccecfdd
Rounak Bhowmick <rounakchakda@gmail.com>
2020-04-21 16:51:14 +0530

    javascript modify

--- reg.js
+++ reg.js
@@ -2,7 +2,8 @@ const form = document.querySelector('.signup-form');
 const namePattern = /^[a-z A-Z]{6,20}$/;
 const addressPattern = /^[a-z A-Z 0-9 \-/]{15,50}$/;
 const emailPattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-const input = document.querySelector('input');
+let c = 0;
+//const input = document.querySelector('input');
 
 
 /*const feedback = document.querySelector('.feedback');
@@ -20,11 +21,41 @@ form.addEventListener('submit', e => {
 });*/
 //live feedback
 //Name
+/*function validateForm() {
+    var x = document.forms["signup-form"]["name"].value;
+    if (x == "" || x == null) {
+        alert("Name must be filled out");
+        return false;
+    }
+}*/
+
 form.name.addEventListener('keyup', e => {
+
     if (namePattern.test(e.target.value)) {
+        c++;
         form.name.setAttribute('class', 'success');
+
     } else {
         form.name.setAttribute('class', 'error');
+    }
+});
+
+//email testing
+form.email.addEventListener('keyup', e => {
+
+    if (emailPattern.test(e.target.value)) {
+        c++;
+        form.email.setAttribute('class', 'success');
+    } else {
+        form.email.setAttribute('class', 'error');
+    }
+});
+
+//Gender
+form.gender.addEventListener('input', e => {
+    if (gender.selectedIndex > 0) {
+        c++;
+
     }
 });
 
@@ -33,11 +64,37 @@ form.name.addEventListener('keyup', e => {
 form.address.addEventListener('keyup', e => {
     if (addressPattern.test(e.target.value)) {
         form.address.setAttribute('class', 'success');
+        c++;
+        alert(c);
     } else {
         form.address.setAttribute('class', 'error');
     }
 });
 
+form.birthday.addEventListener('input', e => {
+    console.log(e.target.value);
+
+});
+
+/*
+function myFunction() {
+    if ((document.getElementById("gender").value) == 'select') {
+        form.address.setAttribute('class', 'error');
+
+    }
+    if ((document.getElementById("birthday").value) == "")
+        alert("fuku")
+    if {
+        var x = document.forms["signup-form"]["name"].value;
+        if (x == "" || x == null) {
+            alert("Name must be filled out");
+
+        }
+    }
+    return false;
+}
+
+/*
 
 /*function ValidateEmail(mail) {
     if (.test(form.email.value)) {
@@ -46,3 +103,16 @@ form.address.addEventListener('keyup', e => {
         form.name.setAttribute('class', 'error');
     }
 }*/
+
+//email testing
+form.email.addEventListener('keyup', e => {
+    if (emailPattern.test(e.target.value)) {
+        form.email.setAttribute('class', 'success');
+    } else {
+        form.email.setAttribute('class', 'error');
+    }
+});
+//submit button
+function validation() {
+    let name = document.getElementById('user').value;
+}
